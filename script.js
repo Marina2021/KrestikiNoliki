043a02a8aa57cb28d69d69d34e10c20445efdb21
Marina Ivanova <m.ivanova@email.cz>
2022-01-19 15:28:52 +0100

    basic clicking

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 for (var i=0; i<9; i++) {
     document.getElementById('game').innerHTML+='<div class="block"><div>';
 } 
+var turn = 0;
+document.getElementById('game').onclick=function(event){
+    if (event.target.className=='block'){
+        if (turn%2==0){
+            event.target.innerHTML='X';
+        }else{
+            event.target.innerHTML='0'
+        }turn++
+    }
+}
