d7a63e455d7980f5ae1ba9304994f47610fcd1a3
Marina Ivanova <m.ivanova@email.cz>
2022-01-21 20:36:10 +0100

    alert removed

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ document.getElementById('game').onclick=function(event){
     if (event.target.className=='block'){
         if (turn%2==0){
             event.target.innerHTML='X';
+            document.getElementById('status').innerHTML="Следующий ход играют Нолики!";
         }else{
-            event.target.innerHTML='0'
+            event.target.innerHTML='0';
+            document.getElementById('status').innerHTML="Следующий ход играют Крестики!";
         }turn++
         checkWinner();
     }
@@ -16,21 +18,21 @@ document.getElementById('game').onclick=function(event){
 function checkWinner(){
     let allblock = document.getElementsByClassName('block');
     //крестики
-    if (allblock[0].innerHTML=='X' && allblock[1].innerHTML=='X' && allblock[2].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[3].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[5].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[6].innerHTML=='X' && allblock[7].innerHTML=='X' && allblock[8].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[0].innerHTML=='X' && allblock[3].innerHTML=='X' && allblock[6].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[1].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[7].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[2].innerHTML=='X' && allblock[5].innerHTML=='X' && allblock[8].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[0].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[8].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
-    if (allblock[2].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[6].innerHTML=='X') alert('Крестики одержали победу! Поздравляем!');
+    if (allblock[0].innerHTML=='X' && allblock[1].innerHTML=='X' && allblock[2].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[3].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[5].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[6].innerHTML=='X' && allblock[7].innerHTML=='X' && allblock[8].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[0].innerHTML=='X' && allblock[3].innerHTML=='X' && allblock[6].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[1].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[7].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[2].innerHTML=='X' && allblock[5].innerHTML=='X' && allblock[8].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[0].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[8].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
+    if (allblock[2].innerHTML=='X' && allblock[4].innerHTML=='X' && allblock[6].innerHTML=='X') document.getElementById('status').innerHTML="Крестики одержали победу! Поздравляем!";
     //нолики
-    if (allblock[0].innerHTML=='0' && allblock[1].innerHTML=='0' && allblock[2].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[3].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[5].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[6].innerHTML=='0' && allblock[7].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[0].innerHTML=='0' && allblock[3].innerHTML=='0' && allblock[6].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[1].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[7].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[2].innerHTML=='0' && allblock[5].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[0].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
-    if (allblock[2].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[6].innerHTML=='0') alert('Нолики одержали победу! Поздравляем!');
+    if (allblock[0].innerHTML=='0' && allblock[1].innerHTML=='0' && allblock[2].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[3].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[5].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[6].innerHTML=='0' && allblock[7].innerHTML=='0' && allblock[8].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[0].innerHTML=='0' && allblock[3].innerHTML=='0' && allblock[6].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[1].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[7].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[2].innerHTML=='0' && allblock[5].innerHTML=='0' && allblock[8].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[0].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[8].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
+    if (allblock[2].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[6].innerHTML=='0') document.getElementById('status').innerHTML="Нолики одержали победу! Поздравляем!";
 }
