5d54b4c967319a1b083b1574fdfbae3f4bc578e0
Marina Ivanova <m.ivanova@email.cz>
2022-01-24 21:21:47 +0100

    draw

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 for (let i=0; i<9; i++) {
     document.getElementById('game').innerHTML+='<div class="block"><div>';
 } 
-
 let turn = 0;
+let gameOver = false;
 document.getElementById('game').onclick=function(event){
-    if (event.target.className=='block'){
-        if (turn%2==0){
+       if (event.target.className=='block' 
+            && event.target.innerHTML=='<div></div>'
+            && gameOver == false){
+            if (turn%2==0){
             event.target.innerHTML='X';
             document.getElementById('status').innerHTML="Следующий ход играют Нолики!";
         }else{
             event.target.innerHTML='0';
             document.getElementById('status').innerHTML="Следующий ход играют Крестики!";
-        }turn++
+        } 
+        turn++;
         checkWinner();
+        if (turn==9) {
+            document.getElementById('status').innerHTML="Ничья! Победила дружба!";
+            gameOver=true;
+        }
     }
 }
 function checkWinner(){
@@ -28,15 +35,21 @@ function checkWinner(){
         [2,4,6]
     ];
     for(i = 0; i < win.length; i++) {
-        if(allblock[win[i][0]].innerHTML == 'X' && allblock[win[i][1]].innerHTML == 'X' && allblock[win[i][2]].innerHTML == 'X'
+        if(allblock[win[i][0]].innerHTML == 'X' 
+        && allblock[win[i][1]].innerHTML == 'X' 
+        && allblock[win[i][2]].innerHTML == 'X'
         ) {
             result = 'крестики';
             prepareResult(result);
-        } else if(allblock[win[i][0]].innerHTML == '0' && allblock[win[i][1]].innerHTML == '0' && allblock[win[i][2]].innerHTML == '0'
+            gameOver = true;
+        } else if(allblock[win[i][0]].innerHTML == '0' 
+        && allblock[win[i][1]].innerHTML == '0' 
+        && allblock[win[i][2]].innerHTML == '0'
         ) {
             result = 'нолики';
             prepareResult(result);
-        }
+            gameOver = true;
+        } 
     }
 }
 
